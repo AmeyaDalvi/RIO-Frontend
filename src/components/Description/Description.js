c41fd90ec7969058bbeb67101eed4d0a030489ce
Pednekar, Aditi Shivaji <apednek@iu.edu>
2023-03-27 18:58:22 -0400

    added product data to product description page

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
-import styles from "../../styles/Home.module.css";
+import styles from "../../styles/description.module.css";
 import { useRouter } from "next/router";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
 
 const url = "localhost:5000/getproduct"
 
@@ -10,14 +12,13 @@ export default function Description() {
     // const { productId } = router.query;
 
     const [product, setProduct] = useState(0);
-    const [productId] = useState(1);
+    const [productId] = useState(3);
     
 
-    useEffect(() => {console.log("test") 
+    useEffect(() => {
         fetchProduct(productId);
         setProduct({})
     }, [productId]);
-    // useEffect(() => {fetchProduct});
 
     let fetchProduct = (productId) => {
         fetch(`http://localhost:5000/getproduct?productid=${productId}`)
@@ -30,17 +31,29 @@ export default function Description() {
 
 
     return(
-        <div className={styles.container}>
-            <div>
-                <h2>{product.Name}</h2>
-                <p>Price: ${product.Price}</p>
-                <p>{product.Description}</p>
+        // <div className={styles.container}>
+            <div className={styles.leftPane}>
+                <Card variant="outlined" style={{ minWidth: 1024, height: 1024 }}
+                    sx={{
+                        // minWidth: "150px",
+                        maxWidth: "400px",
+                        boxShadow: "none",
+                        // border: "2px solid #FF9666",
+                    }}>
+                    <CardContent>
+                        <h1>{product.Name}</h1>
+                        ＄{product.Price}
+                        <div className={styles.para__container}>
+                            {product.Description}
+                        </div>
+                        <div class="card">
+                            <img className={styles.Image} style={{ minWidth: 300, height: 300 }} src={product.ImageURL} alt="Card image cap"/>
+                        </div>
+                    </CardContent>
+                </Card>
+
+            
             </div>
-            {/* {
-                product.ProductId ? (
-                    <h2>{product.name}</h2>
-                ) : null
-            } */}
-        </div>
+        // </div>
     );
 }
