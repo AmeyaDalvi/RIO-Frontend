2d3dc4f38599d9d38fc1f78d20a495fccdfd0038
Pednekar, Aditi Shivaji <apednek@iu.edu>
2023-03-30 00:42:58 -0400

    replaced mapbox-api with google-map-api. Maps work

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -3,6 +3,10 @@ import styles from "../../styles/description.module.css";
 import { useRouter } from "next/router";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
+import dynamic from "next/dynamic";
+// import GM from "components/ProductDescription/GM";
+
+const GM = dynamic(() => import("components/ProductDescription/GM"), { ssr: false });
 
 const url = "localhost:5000/getproduct"
 
@@ -13,6 +17,7 @@ export default function Description() {
 
     const [product, setProduct] = useState(0);
     const [productId] = useState(3);
+    const [address, setAddress] = useState("");
     
 
     useEffect(() => {
@@ -25,13 +30,13 @@ export default function Description() {
         .then((response) => response.json())
         .then((response) => {
         setProduct(response[0]);
-        console.log(productId , response[0]);
+        setAddress(response[0].SIStreet + response[0].SICity + response[0].SIState + response[0].SICountry + response[0].SIZip)
+        // console.log(productId , response[0]);
       });
     };
 
 
     return(
-        // <div className={styles.container}>
             <div className={styles.leftPane}>
                 <Card variant="outlined" style={{ minWidth: 1024, height: 1024 }}
                     sx={{
@@ -46,14 +51,15 @@ export default function Description() {
                         <div className={styles.para__container}>
                             {product.Description}
                         </div>
-                        <div class="card">
+                        <div className="card">
                             <img className={styles.Image} style={{ minWidth: 300, height: 300 }} src={product.ImageURL} alt="Card image cap"/>
                         </div>
+                        {/* <GM location = "1600+Amphitheatre+Parkway,+Mountain+View,+CA,+94043"/> */}
+                        {/* {/* <GM location = {product.SIStreet + ",+" + product.SICity + ",+" + product.SIState + ",+" + product.SIZip + ",+" + product.SICountry}>} */}
+                        <GM location = {product.SIStreet + " " + product.SICity + " " + product.SIState + " " + product.SIZip + " " + product.SICountry}/>
                     </CardContent>
                 </Card>
-
             
             </div>
-        // </div>
     );
 }
