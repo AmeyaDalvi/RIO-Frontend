fb3cf03dd21c0cc18376041ac945608a56dd1e92
Dalvi, Ameya Bhushan <abdalvi@iu.edu>
2023-04-21 20:48:28 -0400

    check this commit

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -7,112 +7,104 @@ import Cookies from "js-cookie";
 import styles from "styles/products.module.css";
 
 const dashboard = () => {
-  
-    const [postedProducts, setPostedProducts] = useState([]);
-    const [purchasedProducts, setPurchasedProducts] = useState([]);
+  const [postedProducts, setPostedProducts] = useState([]);
+  const [purchasedProducts, setPurchasedProducts] = useState([]);
 
-    let userInCookie = Cookies.get("rioUser");
-    userInCookie = userInCookie !== undefined ? JSON.parse(userInCookie) : null;
-    const tokenInCookie = Cookies.get("rioUserToken");
-    const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
-    const [isUser, setIsUser] = useState({});
+  let userInCookie = Cookies.get("rioUser");
+  userInCookie = userInCookie !== undefined ? JSON.parse(userInCookie) : null;
+  const tokenInCookie = Cookies.get("rioUserToken");
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+  const [isUser, setIsUser] = useState({});
 
-    useEffect(() => {
-      checkUserInCookie();
-      getAllUserPostedProducts();
-      getAllUserPurchasedProducts();
-    }, [tokenInCookie]);
+  useEffect(() => {
+    checkUserInCookie();
+    getAllUserPostedProducts();
+    getAllUserPurchasedProducts();
+  }, [tokenInCookie]);
 
-    const checkUserInCookie = () => {
-      console.log("tok", tokenInCookie)
-      if (tokenInCookie) {
-        setIsUserLoggedIn(true);
-        setIsUser(userInCookie);
-        console.log("cookie",userInCookie)
-      } else {
-        setIsUserLoggedIn(false);
-        setIsUser({});
+  const checkUserInCookie = () => {
+    console.log("tok", tokenInCookie);
+    if (tokenInCookie) {
+      setIsUserLoggedIn(true);
+      setIsUser(userInCookie);
+      console.log("cookie", userInCookie);
+    } else {
+      setIsUserLoggedIn(false);
+      setIsUser({});
+    }
+  };
+  const getAllUserPostedProducts = async (userData) => {
+    try {
+      console.log("isuser ", tokenInCookie);
+      console.log("hello", userInCookie["user_id"]);
+      const response = await fetch(
+        baseUrl + "/upposted?id=" + userInCookie["user_id"],
+        {
+          method: "POST",
+          headers: {
+            Authorization: "Bearer " + tokenInCookie,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ emailId: userInCookie["email_id"] }),
+        }
+      );
+      if (response.status === 200) {
+        const data = await response.json();
+        console.log("data", data);
+        setPostedProducts(data);
+      } else if (response.status === 401) {
+        console.log("Unauthorized");
+      } else if (response.status === 403) {
+        console.log("Forbidden");
       }
-    };
-    const getAllUserPostedProducts = async (userData) => {
-        try {
-          console.log("isuser ",tokenInCookie)
-          console.log("hello",  userInCookie["user_id"])
-          const response = await fetch(baseUrl + "/upposted?id=" + userInCookie["user_id"], {
-            method: "POST",
-            headers: {
-              "Authorization": "Bearer " + tokenInCookie,
-              "Content-Type": "application/json"
-            },
-            body: JSON.stringify({emailId: userInCookie["email_id"]}),
-          });
-          if (response.status === 200) {
-            const data = await response.json();
-            console.log("data",data);
-            setPostedProducts(data);
-          } else if (response.status === 401) {
-            console.log("Unauthorized");
-          } else if (response.status === 403) {
-            console.log("Forbidden");
-          }
-        } catch (error) {
-          console.log(error);
-        }
-      };
-    
-      const getAllUserPurchasedProducts = async () => {
-        try {
-          const response = await fetch(baseUrl + "/uppurchased", {
-            method: "GET",
-            headers: {
-              "Authorization": "Bearer " + tokenInCookie,
-              "Content-Type": "application/json"
-            },
-            body: JSON.stringify({emailId: userInCookie["email_id"]}),
-          });
-    
-          if (response.status === 200) {
-            const data = await response.json();
-            console.log(data);
-            setPurchasedProducts(data);
-          } else if (response.status === 401) {
-            console.log("Unauthorized");
-          } else if (response.status === 403) {
-            console.log("Forbidden");
-          }
-        } catch (error) {
-          console.log(error);
-        }
-      };
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    return (
-      <div className={styles.container}>
-        <div className={styles.products}>
+  const getAllUserPurchasedProducts = async () => {
+    try {
+      const response = await fetch(baseUrl + "/uppurchased", {
+        method: "POST",
+        headers: {
+          Authorization: "Bearer " + tokenInCookie,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ emailId: userInCookie["email_id"] }),
+      });
+
+      if (response.status === 200) {
+        const data = await response.json();
+        console.log("purchased", data);
+        setPurchasedProducts(data);
+      } else if (response.status === 401) {
+        console.log("Unauthorized");
+      } else if (response.status === 403) {
+        console.log("Forbidden");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.products}>
         <h2>You Posted</h2>
-          {postedProducts.length !== 0 ? (
-            <List
-              input=""
-              rating= "-1"
-              price="-1"
-              products={postedProducts}
-            />
-          ) : (
-            "None"
-          )}
+        {postedProducts.length !== 0 ? (
+          <List input="" rating="-1" price="-1" products={postedProducts} />
+        ) : (
+          "There are no posted products by user"
+        )}
 
         <h2>You Purchased</h2>
-          {purchasedProducts.length !== 0 ? (
-            <List
-              input=""
-              rating= "-1"
-              price="-1"
-              products={purchasedProducts}
-            />
-          ) : (
-            "None"
-          )}
-        </div>
+        {purchasedProducts.length !== 0 ? (
+          <List input="" rating="-1" price="-1" products={purchasedProducts} />
+        ) : (
+          "There are no purchased products by user"
+        )}
       </div>
-    );
-  };
-  export default dashboard;
+    </div>
+  );
+};
+export default dashboard;
